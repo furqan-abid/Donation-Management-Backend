d90a800f179db4df622bd760f7c3a1545a6bd1b8
furqan-abid <furqanabid10@gmail.com>
2023-10-09 13:03:58 +0500

    delete investor

--- controllers/investorContorller.ts
+++ controllers/investorContorller.ts
@@ -68,7 +68,8 @@ export const getsingleInvestor = catchAsyncError(async (req,res,next)=>{
 })
 
 export const deleteInvestor = catchAsyncError(async (req,res,next)=>{
-    const deletedInvestor=await investorModel.deleteOne({"_id": req.params._id });
+    // const deletedInvestor=await investorModel.deleteOne({"_id": req.params._id });
+    const deletedInvestor=await investorModel.findByIdAndDelete(req.params._id );
     res.status(200).json({
         success:true,
         message:"investor deleted successfully",
--- routes/investorRoutes.ts
+++ routes/investorRoutes.ts
@@ -1,10 +1,11 @@
 import express from 'express'
-import { getAllInvestors, getsingleInvestor, investFunds } from '../controllers/investorContorller'
+import { deleteInvestor, getAllInvestors, getsingleInvestor, investFunds } from '../controllers/investorContorller'
 
 const router = express.Router()
 
 router.route("/investor/investfunds").post(investFunds)
 router.route("/investor/all").get(getAllInvestors)
 router.route("/investor/single/:id").get(getsingleInvestor)
+router.route("/investor/delete/:id").delete(deleteInvestor)
 
 export default router
